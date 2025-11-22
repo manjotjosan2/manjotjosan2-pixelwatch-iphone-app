aeae29836f9e95ab117a60193d7934bbdecc9886
manjotjosan2 <manjotjosan2@gmail.com>
2025-11-22 14:29:56 -0800

    Add HeartRateService class for processing heart rate data

---
+++ Sensors/HeartRateService.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class HeartRateService {
+    func processHeartRateData(_ value: Int) {
+        // Handle heart rate sample (update dashboard, send to paired watch, etc)
+    }
+}
