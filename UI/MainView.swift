40c6ae80891a5b68b04fbafaad6db0607ee4ae33
manjotjosan2 <manjotjosan2@gmail.com>
2025-11-22 14:28:53 -0800

    Create MainView for Pixel Watch iPhone App

---
+++ UI/MainView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Pixel Watch iPhone App")
+                .font(.title)
+                .padding()
+            // Add more UI components here!
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
