e7a942f28d8652cfbcb055406abead289c73eb2b
manjotjosan2 <manjotjosan2@gmail.com>
2025-11-22 17:53:22 -0800

    Add syncHeartRate method to HeartRateService
    Added a method to sync heart rate data to a watch.

--- Sensors/HeartRateService.swift
+++ Sensors/HeartRateService.swift
@@ -4,4 +4,7 @@ class HeartRateService {
     func processHeartRateData(_ value: Int) {
         // Handle heart rate sample (update dashboard, send to paired watch, etc)
     }
+    func syncHeartRate(_ value: Int) {
+    // Send heart rate to watch over BLE using protocol spec
+}
 }
